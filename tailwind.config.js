5a0601fcef5cb2685a1a28d9813cd9a286da1d7a
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2024-08-25 18:11:24 +0600

    nav

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        'bg-green-0': '#ABEF5F',
+      },
+    },
+  }
